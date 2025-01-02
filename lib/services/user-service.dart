334f62ecbfd836a513a4bc4a9fc9485e3bb09fa3
Kemal Guneysu <kemalguneysu5@gmail.com>
2025-01-02 22:28:18 +0300

    hesap oluşturam sırasında hata olursa toast ile gösteriliyor

--- lib/services/user-service.dart
+++ lib/services/user-service.dart
@@ -10,23 +10,50 @@ class UserService {
   // dotenv.env['API_BASE_URL'] ??
     final String apiBaseUrl = 'https://api.denemetakip.com/api';
 
-    Future<Map<String, dynamic>?> createUser(Map<String, dynamic> user,BuildContext context,{VoidCallback? callBackFunction}) async {
-      final response = await http.post(
-        Uri.parse('$apiBaseUrl/users/createUser'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode(user),
-      );
-      final responseData = jsonDecode(response.body);
-      if (responseData['succeeded'] == true) {
+    Future<Map<String, dynamic>?> createUser(
+    Map<String, dynamic> user,
+    BuildContext context, {
+    VoidCallback? callBackFunction,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$apiBaseUrl/users/createUser'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(user),
+    );
+
+    final responseData = jsonDecode(response.body);
+
+    if (responseData is List) {
+      // Hataları işleme
+      String errorMessage = '';
+      for (var error in responseData) {
+        if (error['key'] != null && error['value'] != null) {
+          String key = error['key'];
+          String values = (error['value'] as List).join("");
+          errorMessage += "$values\n";
+        }
+      }
+      errorToast("Hata", errorMessage.trim(), context);
+      return null; // Başarısız durumda geriye bir şey döndürmüyoruz
+    } else if (responseData is Map<String, dynamic> &&
+        responseData['succeeded'] == true) {
+      // Başarılı yanıt
       if (callBackFunction != null) {
         callBackFunction();
       }
       return responseData;
-      } 
-    else {
-      errorToast("Hata",
-          responseData['message'] ?? 'Bilinmeyen bir hata oluştu', context);
-      }
+    } else if (responseData is Map<String, dynamic>) {
+      // Beklenmeyen bir durum
+      errorToast(
+        "Hata",
+        responseData['message'] ?? 'Bilinmeyen bir hata oluştu',
+        context,
+      );
+      return null;
+    } else {
+      errorToast("Hata", "Beklenmeyen bir yanıt alındı", context);
+      return null;
     }
+  }
 
 }
